2af0af8d71a4bf24e28386880ecd67288de517da
Nebojsa Konsta <nebojsa.konsta@gmail.com>
2026-05-19 17:21:09 +0200

    bump to 0.8.35

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,7 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    version: "0.8.34",
+    version: "0.8.35",
     settings: {
       evmVersion: "osaka",
       viaIR: true,
